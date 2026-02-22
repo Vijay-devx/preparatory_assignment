bdf1d713298b62d44f10308320a914d9e018d425
Vijay-devx <yadavvijay3107@gmail.com>
2026-02-22 08:44:38 +0530

    added Q13_StringDuplicate.java

--- Q13_StringDuplicate.java
+++ Q13_StringDuplicate.java
@@ -10,5 +10,31 @@ import java.util.Scanner;
 
 public class Q13_StringDuplicate
 {
+     static void duplicate(String[] arr)
+     {
+          int hold = 0;
 
+          System.out.println("Duplicate Strings are: ");
+
+          for(int move = 1; move<arr.length; move++)
+          {
+               if(arr[hold].equals(arr[move]))
+               {
+                    System.out.println(arr[hold]);
+
+                    hold++;
+               }
+          }
+     }
+
+     public static void main(String[] args)
+     {
+          Scanner sc = new Scanner(System.in);
+
+          String[] arr = {"apple", "banana", "mango", "apple", "orange", "banana", "pineapple"};
+
+          duplicate(arr);
+
+          sc.close();
+     }
 }
